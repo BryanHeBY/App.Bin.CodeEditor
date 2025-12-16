a81c534bc381d1bd5985702052a208aff576c023
flex_7746 <toulanren@qq.com>
2025-12-16 18:05:17 +0800

    feat: 读取目录接口

--- backend/src/utils/exec.js
+++ backend/src/utils/exec.js
@@ -3,6 +3,7 @@ const getType = require("./type");
 const router = {
   read: { type: "file", run: require("../router/read") },
   save: { run: require("../router/save") },
+  dir: { run: require("../router/dir") },
 };
 
 module.exports = async function exec(data) {
