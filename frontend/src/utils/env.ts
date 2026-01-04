1167dd4f9cad5b59ce20e467a9beeb0d3fb5eb33
flex_7746 <toulanren@qq.com>
2026-01-04 23:35:33 +0800

    feat: 部分细节优化

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -1,11 +1,9 @@
 export const IS_DEV = import.meta.env.MODE === 'development'
 
-<<<<<<< HEAD
 export const HOST = IS_DEV ? 'http://127.0.0.1:17746' : '/cgi/ThirdParty/code-editor-hby/index.cgi'
-=======
-export const HOST = IS_DEV ? 'http://127.0.0.1:17746' : '/cgi/ThirdParty/code.editor/index.cgi'
 
 export const USER_CONFIG_PATH = IS_DEV
   ? '/Users/flex/Downloads/config.json'
-  : '/var/apps/code.editor/shares/code.editor/config.json'
->>>>>>> 25d4c12cfda9c74fd4888035a98a65a84bc08ada
+  : '/var/apps/code-editor-hby/shares/code-editor-hby/config.json'
+
+export const APP_DIR_PATH = IS_DEV ? '/Users/flex/Downloads' : '/var/apps/code-editor-hby/shares/code-editor-hby'
