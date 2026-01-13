a63a95e5e9ad75eed0dfb37c85d889c1d67018b1
BryanHeBY <bryanheby@qq.com>
2026-01-13 20:37:21 +0800

    hby change

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -2,10 +2,6 @@ export const IS_DEV = import.meta.env.MODE === 'development'
 
 export const HOST = IS_DEV ? 'http://127.0.0.1:17746' : '/cgi/ThirdParty/code-editor-hby/index.cgi'
 
-export const USER_CONFIG_PATH = IS_DEV
-  ? '/Users/flex/Downloads/config.json'
-  : '/var/apps/code-editor-hby/shares/code-editor-hby/config.json'
-
 export const APP_DIR_PATH = IS_DEV ? '/Users/flex/Downloads' : '/var/apps/code-editor-hby/shares/code-editor-hby'
 
 export const USER_CONFIG_PATH = IS_DEV
